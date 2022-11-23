95026ce76b873fa0a15de44b7465f0997b9165ef
Nico Pedernera <nicopedernera@hotmail.com.ar>
2022-11-23 19:19:17 -0300

    Commit clase 3

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { game } from '../game';
 
 @Component({
   selector: 'app-game-list',
@@ -8,14 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class GameListComponent implements OnInit {
 
   constructor() { }
-  game = {
-    nombre : "Uncharted 4",
-    precio: "500",
-    desarrollador : "Naugthy Dog",
-    image: "assets/uncharted.jpg",
-    stock : "40"
-
-  }
+  games : game [] = [
+    {
+      nombre : "Uncharted 4",
+      precio: 500,
+      desarrollador : "Naugthy Dog",
+      image: "assets/uncharted.jpg",
+      stock : 40,
+      oferta : true,
+    },
+    {
+      nombre : "Fifa 23",
+      precio: 600,
+      desarrollador : "Electronics Arts",
+      image: "assets/fifa.jpg",
+      stock : 23,
+      oferta : true,
+    },
+    {
+      nombre : "Spider-Man",
+      precio: 450,
+      desarrollador : "Insomniac Games",
+      image: "assets/spiderman.jpg",
+      stock : 0,
+      oferta : false,
+    }
+  ]
   ngOnInit(): void {
   }
 
---
+++ src/app/game.ts
@@ -0,0 +1,8 @@
+export interface game{
+    nombre : String,
+    precio: number,
+    desarrollador : string,
+    image: string,
+    stock : number,
+    oferta : boolean
+}
